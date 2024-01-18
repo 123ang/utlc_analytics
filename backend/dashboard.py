ec7de29607e194c411613841665f4dd1e607b6da
123ang <angjinsheng@gmail.com>
2024-01-19 06:00:45 +0800

    updates

--- backend/dashboard.py
+++ backend/dashboard.py
@@ -10,6 +10,7 @@ class Dashboard:
     def setup_routes(self):
         @self.app.route('/dashboard/staff_count_by_school', methods=['GET'])
         def staff_count_by_school():
+            # bar chart
             query = """
                 SELECT S.name_eng, COUNT(L.id) as count
                 FROM LECTURERS L
@@ -22,6 +23,7 @@ class Dashboard:
 
         @self.app.route('/dashboard/staff_position_breakdown', methods=['GET'])
         def staff_position_breakdown():
+            # pie chart
             query = """
                 SELECT actual_position, COUNT(*) as count 
                 FROM LECTURERS 
@@ -33,6 +35,7 @@ class Dashboard:
 
         @self.app.route('/dashboard/joining_dates_timeline', methods=['GET'])
         def joining_dates_timeline():
+            # bar chart / line graph
             query = """
                 SELECT DATE(entering_uum_date) as date, COUNT(*) as count 
                 FROM LECTURERS 
@@ -44,6 +47,7 @@ class Dashboard:
 
         @self.app.route('/dashboard/staff_join_per_year', methods=['GET'])
         def staff_join_per_year():
+            # column chart
             query = """
                 SELECT YEAR(entering_uum_date) as year, COUNT(*) as count 
                 FROM LECTURERS 
@@ -52,3 +56,43 @@ class Dashboard:
             result = fetch_data(query)
             join_per_year = {item['year']: item['count'] for item in result}
             return jsonify(join_per_year)
+
+
+        @self.app.route('/dashboard/training_count_by_school', methods=['GET'])
+        def training_count_by_school():
+            # bar chart
+            query = """
+                        SELECT S.name_eng, COUNT(T.id) as count
+                        FROM TRAININGS T
+                        JOIN SCHOOLS S ON T.school_id = S.id
+                        GROUP BY S.name_eng
+                    """
+            result = fetch_data(query)
+            training_count_by_school = {item['name_eng']: item['count'] for item in result}
+            return jsonify(training_count_by_school)
+
+        @self.app.route('/dashboard/lecturer_attendance_in_trainings', methods=['GET'])
+        def lecturer_attendance_in_trainings():
+            # bar chart
+            query = """
+                        SELECT T.name_english, COUNT(TL.lecturer_id) as count
+                        FROM TRAINING_LECTURER TL
+                        JOIN TRAININGS T ON TL.training_id = T.id
+                        GROUP BY T.name_english
+                    """
+            result = fetch_data(query)
+            lecturer_attendance = {item['name_english']: item['count'] for item in result}
+            return jsonify(lecturer_attendance)
+
+        @self.app.route('/dashboard/trainings_over_time', methods=['GET'])
+        def trainings_over_time():
+            # line graph
+            query = """
+                        SELECT DATE(start_date) as date, COUNT(*) as count
+                        FROM TRAININGS
+                        GROUP BY DATE(start_date)
+                        ORDER BY DATE(start_date)
+                    """
+            result = fetch_data(query)
+            trainings_over_time = {item['date']: item['count'] for item in result}
+            return jsonify(trainings_over_time)
--- backend/import_data.py
+++ backend/import_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from database import execute_command,fetch_data
-
-# Load the CSV data into a DataFrame
+from datetime import datetime
+#
+# # Load the CSV data into a DataFrame
 df = pd.read_csv('../dataset/staff_info.csv', encoding='utf-8')
 df = df.rename(columns={
-    'No Staf': 'id',
+    'No Staf': 'staff_id',
     'Nama Staf dan gelaran': 'name',
     'TARIKH PROSES DATA STAF': 'process_data_staff_date',
     'Status Perkhidmatan': 'status',
@@ -17,6 +18,15 @@ df = df.rename(columns={
     'Pusat Pengajian': 'short_name'
 })
 df = df[df['short_name'] != 'Lain-lain']
+
+
+def convert_date(date_str):
+    try:
+        return datetime.strptime(date_str, '%m/%d/%Y').strftime('%Y-%m-%d')
+    except ValueError as e:
+        # Handle the error or return a default value
+        print(f"Error converting date: {e}")
+        return None
 
 
 def insert_school(school_name):
@@ -55,7 +65,7 @@ def check_if_lecturer_exists(lecturer_id):
 
 def insert_or_update_lecturer(row):
     try:
-        lecturer_id = str(row['id'])
+        lecturer_id = str(row['staff_id'])
         exists = check_if_lecturer_exists(lecturer_id)
 
         school_id = str(get_school_id(row['short_name']))
@@ -65,7 +75,7 @@ def insert_or_update_lecturer(row):
                     UPDATE lecturers SET name = %s, process_data_staff_date = %s, status = %s, ic = %s, 
                     entering_uum_date = %s, actual_position = %s, current_position = %s, 
                     current_position_short = %s, school_id = %s, updated_by = 'system', updated_at = NOW()
-                    WHERE id = %s
+                    WHERE staff_id = %s
                 """
                 data_to_update = (
                     str(row['name']),
@@ -83,7 +93,7 @@ def insert_or_update_lecturer(row):
                 return "Updated existing record"
             else:
                 insert_query = """
-                    INSERT INTO lecturers (id, name, process_data_staff_date, status, ic, entering_uum_date, actual_position,
+                    INSERT INTO lecturers (staff_id, name, process_data_staff_date, status, ic, entering_uum_date, actual_position,
                     current_position, current_position_short, school_id, created_by, updated_by, created_at, updated_at)
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 'system', 'system', NOW(), NOW())
                 """
@@ -109,17 +119,78 @@ def insert_or_update_lecturer(row):
     except Exception as e:
         print(f"An error occurred while inserting or updating the lecturer: {e}")
         return None
+
+
 # Use the function to insert each distinct school name
-# distinct_schools = df[df['short_name'] != 'Lain-lain']['short_name'].unique()
-# for school in distinct_schools:
-#     if pd.notna(school):  # This will check if the value is not NaN
-#         insert_school(school)
+distinct_schools = df[df['short_name'] != 'Lain-lain']['short_name'].unique()
+for school in distinct_schools:
+    if pd.notna(school):  # This will check if the value is not NaN
+        insert_school(school)
 
 i = 1
 for _, row in df.iterrows():
-    # row = df.iloc[1]
-    print(row)
     result = insert_or_update_lecturer(row)
     i+=1
-    print(i)
-    print(result)
+
+
+
+# Read the data into a pandas DataFrame
+# df = pd.read_csv('../dataset/training_data.csv', encoding='utf-8')
+# # Preprocess the DataFrame by forward filling the missing values
+# columns_to_fill = ['Kod_PP', 'NO. PEKERJA', 'NAMA', 'JAWATAN AKADEMIK','TARIKH MULA', 'TARIKH AKHIR']
+# df[columns_to_fill] = df[columns_to_fill].ffill()
+#
+# # Save the preprocessed data to a new CSV file
+# preprocessed_file_path = '../dataset/preprocessed_training_data.csv'  # Replace with your desired path
+# df.to_csv(preprocessed_file_path, index=False)
+
+df = pd.read_csv('../dataset/preprocessed_training_data.csv', encoding='utf-8')
+df['TARIKH MULA'] = df['TARIKH MULA'].apply(convert_date)
+df['TARIKH AKHIR'] = df['TARIKH AKHIR'].apply(convert_date)
+
+
+# Insert distinct trainings into the TRAININGS table
+distinct_trainings = df.drop_duplicates(subset=['NAMA BENGKEL'], keep='first')
+
+
+for index, training in distinct_trainings.iterrows():
+    insert_query = """
+        INSERT INTO TRAININGS (start_date, end_date, name_english, created_by, created_at, updated_by, updated_at)
+        VALUES (%s, %s, %s, 'system', NOW(), 'system', NOW())
+    """
+    execute_command(insert_query, (
+        training['TARIKH MULA'],
+        training['TARIKH AKHIR'],
+        training['NAMA BENGKEL']
+    ))
+
+
+# Function to get the training_id from TRAININGS table
+def fetch_training_id(name_english):
+    query = "SELECT id FROM TRAININGS WHERE name_english = %s"
+    result = fetch_data(query, (name_english,))
+    return result[0] if result else None
+
+
+def fetch_lecturer_id(staff_id):
+    query = "SELECT id FROM LECTURERS WHERE staff_id = %s"
+    result = fetch_data(query, (staff_id,))
+    return result[0] if result else None
+
+
+# Insert data into the TRAINING_LECTURER table
+for index, row in df.iterrows():
+    lecturer_id = fetch_lecturer_id(row['NO. PEKERJA'])
+    training_id = fetch_training_id(row['NAMA BENGKEL'])
+    if lecturer_id and training_id:
+        insert_query = """
+            INSERT INTO TRAINING_LECTURER (training_id, lecturer_id, created_by, created_at, updated_by, updated_at)
+            VALUES (%s, %s, 'system', NOW(), 'system', NOW())
+        """
+        execute_command(insert_query, (
+            training_id,
+            lecturer_id
+        ))
+
+print("done")
+
