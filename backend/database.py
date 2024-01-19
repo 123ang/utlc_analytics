c7b9d5b730024c005763a7f99b71b1cfa20add41
123ang <angjinsheng@gmail.com>
2024-01-19 08:28:31 +0800

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="",
+        passwd="920214",
         database="utlc_datawarehouse"
     )
 
